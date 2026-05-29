d2e300f9a2b14b229856d053d6ca17249b97b5f3
vamppdevv1 <pacounte@gmail.com>
2026-05-29 03:20:24 +0100

    adding features to home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,21 @@
 import { Navbar } from "../../Components/navbar/Navbar";
 import { Header } from "../../Components/header/Header";
+import { Featured } from "../../Components/featured/Featured";
+import { PropertyList } from "../../Components/propertyList/PropertyList";
+import { FeaturedProperies } from "../../Components/featuredProperties/FeaturedProperies";
 import "./home.css";
 export const Home = () => {
   return (
     <div>
       <Navbar />
       <Header />
+      <div className="homeContainer">
+        <Featured />
+        <h1 className="homeTitle">Browse by property type</h1>
+        <PropertyList/>
+        <h1 className="homeTitle">Guests love</h1>
+<FeaturedProperies/>
+      </div>
     </div>
   );
 };
